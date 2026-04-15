7d8362b99e703e1090104e236fd1b295a50bc42e
Natametonini <118022758+Natametonini@users.noreply.github.com>
2026-04-14 21:04:54 -0300

    Create Funcionario superclass

---
+++ src/com/caps/rh/Funcionario.java
@@ -0,0 +1,26 @@
+package com.caps.rh;
+
+public class Funcionario {
+    // Atributos
+    private String nome, cpf;
+    private double salario;
+
+    // Construtor
+    public Funcionario(String nome, String cpf, double salario) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.salario = salario; 
+    }
+
+    // Getters
+    public String getNome() { return nome; }
+    public String getCpf() { return cpf; }
+    public double getSalario() { return salario; }
+
+    public double aplicarAumento(double percentual) {
+        if (percentual > 0) {
+            salario = salario + (salario * (percentual / 100));
+        }
+        return salario;
+    }
+}
--- src/com/caps/rh/Main.java
+++ src/com/caps/rh/Main.java
@@ -1,5 +1,6 @@
+package com.caps.rh;
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        
     }
 }
